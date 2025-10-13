42a5343411570b4a39a3a7882f146d70eeab9121
gems85 <gelene85@gmail.com>
2025-10-13 17:06:32 -0400

    Update upload-photo.js

--- api/upload-photo.js
+++ api/upload-photo.js
@@ -56,7 +56,7 @@ export default async function handler(req, res) {
             {
                 method: 'POST',
                 headers: {
-                    'Authorization': `Bearer ${AIRTABLE_PAT}`,
+                    'Authorization': `Bearer ${AIRTABLE_TOKEN}`,
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(photoRecord)
