8bb82ddcbcb9dee7b46a63442d1faafc9ecabd92
gems85 <gelene85@gmail.com>
2025-10-14 01:30:13 -0400

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(
-            `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}?view=viwVayhjLGZ8osbLe`,
+            `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}?view=Grid%20view`,
             {
                 headers: {
                     'Authorization': `Bearer ${AIRTABLE_TOKEN}`,
