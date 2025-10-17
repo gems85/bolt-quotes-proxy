4dde9d448286950337e00dc922df951bd7ed6444
gems85 <gelene85@gmail.com>
2025-10-17 16:30:32 -0400

    Update update-project-status.js

--- api/update-project-status.js
+++ api/update-project-status.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     }
 
     const AIRTABLE_TOKEN = process.env.AIRTABLE_PAT;
-    const BASE_ID = 'applWK4PXoo86ajvD';
+    const BASE_ID = process.env.BASE_ID;
     const PROJECTS_TABLE = 'tblhfewGPP306LVNX';
 
     if (!AIRTABLE_TOKEN) {
