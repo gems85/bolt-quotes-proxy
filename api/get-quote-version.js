b4da39cc5b7a39e94bc52c80dba938ea65d1d78e
gems85 <gelene85@gmail.com>
2025-11-06 11:49:59 -0500

    Update get-quote-version.js

--- api/get-quote-version.js
+++ api/get-quote-version.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     if (req.method === 'OPTIONS') {
